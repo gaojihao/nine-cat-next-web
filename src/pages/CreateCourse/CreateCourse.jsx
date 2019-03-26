e073ca716142086c940b39a190b71a0236524c71
lizhi <lizhi1026@126.com>
2019-03-26 23:13:00 +0800

    add course create

---
+++ src/pages/CreateCourse/CreateCourse.jsx
@@ -0,0 +1,27 @@
+
+
+import React, { Component } from 'react';
+import CustomBreadcrumb from '../../components/CustomBreadcrumb';
+
+import './CreateCourse.scss';
+
+export default class CreateCourse extends Component {
+  static displayName = 'CreateCourse';
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    const breadcrumb = [
+      { text: '课程管理', link: '' },
+      { text: '添加课程', link: '#/cate/list' },
+    ];
+    return (
+      <div className="create-course-page">
+        <CustomBreadcrumb dataSource={breadcrumb} />
+      </div>
+    );
+  }
+}
---
+++ src/pages/CreateCourse/index.js
@@ -0,0 +1,3 @@
+import CreateCourse from './CreateCate';
+
+export default CreateCourse;
