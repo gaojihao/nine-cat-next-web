1db61438af796d16a6ef30e025189b17081ac0da
gaojihao <lizhi1026@126.com>
2025-03-10 20:29:04 +0800

    update

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"lEOaexAPdarSPH79nVdwZGNBEZ7170oD0UlQcx+i04M=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
